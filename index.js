abcb188a32d4c4ab3e9eba263ca594c4d9524adc
Kassandra <K1995Moreno@gmail.com>
2022-11-02 12:28:26 -0400

    commit tree 1e9dcbd7e41ac735b6cb4af0fb41db79633c36a7

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+        //grabbing
+        const input = document.getElementById('button')
+    
+        //creating alert
+        const clickAlert= () => alert('I was clicked!')
+    
+        //adding event listener
+        input.addEventListener('click', clickAlert);
 }
